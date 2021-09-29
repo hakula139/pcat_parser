94dce63c44c096cf1852112b0c271e2c57e6c342
hakula139 <1517242376@qq.com>
2021-09-29 12:09:52 +0800

    feat: modify output path

---
+++ src/config.hpp
@@ -0,0 +1,6 @@
+#ifndef SRC_CONFIG_HPP_
+#define SRC_CONFIG_HPP_
+
+constexpr const char* output_dir = "output";
+
+#endif  // SRC_CONFIG_HPP_
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,32 @@
 #include <FlexLexer.h>
 
+#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <memory>
 
+#include "config.hpp"
 #include "io_buffer.hpp"
 #include "lexer.hpp"
 
-constexpr const char* output_path = "output/result.txt";
+namespace fs = std::filesystem;
 
 int main(int argc, char** argv) {
-  const char* input_path = argc > 1 ? argv[1] : nullptr;
-  IOBuffer buf{input_path, output_path};
+  auto buf = [argc, argv]() {
+    if (argc == 1) return IOBuffer{};
+
+    fs::path input_path = argv[1];
+    std::cout << "Input path: " << input_path.c_str() << "\n";
+    fs::path output_path = output_dir /
+                           (input_path.filename().replace_extension(".txt"));
+    std::cout << "Output path: " << output_path.c_str() << "\n";
+    return IOBuffer{input_path.c_str(), output_path.c_str()};
+  }();
 
   auto lexer = std::make_unique<yyFlexLexer>(buf.yyin(), buf.yyout());
 
   int c = T_EOF;
   while ((c = lexer->yylex()) != T_EOF) {
-    buf.yyout() << "<" << lexer->YYText() << ">";
+    buf.yyout() << lexer->lineno() << ": " << lexer->YYText() << "\n";
   }
 }
