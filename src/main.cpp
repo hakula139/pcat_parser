cc2b69d904832da8e9c2639ce721fcf805a5d5b0
Hakula Chen <i@hakula.xyz>
2021-11-28 03:55:10 +0800

    refactor: discard io_buffer
    As std::ifstream and std::ofstream can automatically close themselves after destruction, there's no need to explicitly call the close() method.

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,27 @@
-#include <filesystem>  // fs::path
+#include <filesystem>  // std::filesystem::path
 
 #include "base/config.hpp"
 #include "driver.hpp"
-#include "utils/io_buffer.hpp"
 #include "utils/logger.hpp"
 
 namespace fs = std::filesystem;
 
 int main(int argc, char** argv) {
-  // For self-destruction when exceptions occurred.
-  auto buf = [argc, argv]() {
-    if (argc == 1) return IOBuffer{};
+  if (argc == 1) {
+    Logger::Fatal("no input path specified");
+    return EXIT_FAILURE;
+  }
 
-    const auto IN_PATH = fs::path{argv[1]};
-    Logger::Info("input path: " + IN_PATH.string());
+  const auto in_path = fs::path{argv[1]};
+  Logger::Info("input path: " + in_path.string());
 
-    const auto OUT_FILENAME = IN_PATH.filename().replace_extension(".txt");
-    const auto OUT_PATH = OUTPUT_DIR / OUT_FILENAME;
-    Logger::Info("output path: " + OUT_PATH.string());
+  const auto out_file = in_path.filename().replace_extension(".txt");
+  const auto out_path = OUTPUT_DIR / out_file;
+  Logger::Info("output path: " + out_path.string());
 
-    return IOBuffer{IN_PATH.c_str(), OUT_PATH.c_str()};
-  }();
-
+  Logger::Info("parse started");
   Driver drv;
-  int ret = drv.Parse(buf.input(), buf.output());
+  int ret = drv.Parse(in_path, out_path);
   Logger::Info("parse complete, return code: " + std::to_string(ret));
   return ret;
 }
