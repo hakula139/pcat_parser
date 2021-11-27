8a48a7cc494e95b7c8b5d46654b21d28cd215d0b
Hakula Chen <i@hakula.xyz>
2021-11-28 01:32:56 +0800

    feat: add location info to logs

--- src/utils/logger.cpp
+++ src/utils/logger.cpp
@@ -4,6 +4,7 @@
 #include <string>
 
 #include "../base/config.hpp"
+#include "../location.hpp"
 
 #define RED "\e[0;31m"
 #define GREEN "\e[0;32m"
@@ -12,41 +13,60 @@
 #define PURPLE "\e[0;35m"
 #define RESET "\e[0;0m"
 
-inline void Logger::Log(const std::string& msg, std::ostream& output) {
+inline void Logger::Log(
+    const std::string& msg,
+    const location_type& loc = {},
+    std::ostream& output) {
+  if (loc != location_type{}) output << loc << ": ";
   output << msg << "\n";
 }
 
-inline void Logger::Debug(const std::string& msg, std::ostream& output) {
+inline void Logger::Debug(
+    const std::string& msg,
+    const location_type& loc = {},
+    std::ostream& output) {
   if (LOG_LEVEL > DEBUG) return;
   output << GREEN << "[DEBUG] ";
-  Log(msg, output);
+  Log(msg, loc, output);
   output << RESET;
 }
 
-inline void Logger::Info(const std::string& msg, std::ostream& output) {
+inline void Logger::Info(
+    const std::string& msg,
+    const location_type& loc = {},
+    std::ostream& output) {
   if (LOG_LEVEL > INFO) return;
   output << BLUE << "[INFO ] ";
-  Log(msg, output);
+  Log(msg, loc, output);
   output << RESET;
 }
 
-inline void Logger::Warn(const std::string& msg, std::ostream& output) {
+inline void Logger::Warn(
+    const std::string& msg,
+    const location_type& loc = {},
+    std::ostream& output) {
   if (LOG_LEVEL > WARN) return;
   output << YELLOW << "[WARN ] ";
-  Log(msg, output);
+  Log(msg, loc, output);
   output << RESET;
 }
 
-inline void Logger::Error(const std::string& msg, std::ostream& output) {
+inline void Logger::Error(
+    const std::string& msg,
+    const location_type& loc = {},
+    std::ostream& output) {
   if (LOG_LEVEL > ERROR) return;
   output << RED << "[ERROR] ";
-  Log(msg, output);
+  Log(msg, loc, output);
   output << RESET;
 }
 
-inline void Logger::Fatal(const std::string& msg, std::ostream& output) {
+inline void Logger::Fatal(
+    const std::string& msg,
+    const location_type& loc = {},
+    std::ostream& output) {
   if (LOG_LEVEL > FATAL) return;
   output << PURPLE << "[FATAL] ";
-  Log(msg, output);
+  Log(msg, loc, output);
   output << RESET;
 }
--- src/utils/logger.hpp
+++ src/utils/logger.hpp
@@ -4,16 +4,40 @@
 #include <iostream>
 #include <string>
 
+#include "../location.hpp"
+
 class Logger {
  public:
-  static void Debug(const std::string& msg, std::ostream& output = std::cout);
-  static void Info(const std::string& msg, std::ostream& output = std::cout);
-  static void Warn(const std::string& msg, std::ostream& output = std::cout);
-  static void Error(const std::string& msg, std::ostream& output = std::cout);
-  static void Fatal(const std::string& msg, std::ostream& output = std::cout);
+  static void Debug(
+      const std::string& msg,
+      const location_type& loc = {},
+      std::ostream& output = std::cout);
+
+  static void Info(
+      const std::string& msg,
+      const location_type& loc = {},
+      std::ostream& output = std::cout);
+
+  static void Warn(
+      const std::string& msg,
+      const location_type& loc = {},
+      std::ostream& output = std::cout);
+
+  static void Error(
+      const std::string& msg,
+      const location_type& loc = {},
+      std::ostream& output = std::cout);
+
+  static void Fatal(
+      const std::string& msg,
+      const location_type& loc = {},
+      std::ostream& output = std::cout);
 
  protected:
-  static void Log(const std::string& msg, std::ostream& output = std::cout);
+  static void Log(
+      const std::string& msg,
+      const location_type& loc = {},
+      std::ostream& output = std::cout);
 };
 
 #endif  // SRC_UTILS_LOGGER_HPP_
